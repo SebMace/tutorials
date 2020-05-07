9b8a0a1b35d58e4c3dcfcc083ccd13acdb0b141f
SebMace <sebmace78@gmail.com>
2020-05-07 21:08:16 +0200

    log4j2-examples

--- log4j2-example/src/main/java/org/example/App.java
+++ log4j2-example/src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        // let's pretend we're logging!
+        System.out.println( "Hello user!" );
+        // let's pretend we're logging again!
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
     }
 }
